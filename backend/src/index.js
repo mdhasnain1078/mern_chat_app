4ee646b3dbbbdd19efb712461ab58aea4ff7fe24
md <hasnainshaikh62479@gmail.com>
2024-04-28 14:48:48 +0530

    fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -71,10 +71,10 @@ connectDB()
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname1, "/frontend/dist")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "../frontend/dist/index.html"))
+    res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {

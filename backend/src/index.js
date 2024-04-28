40d972e46ccc6ea1dd3d25937baf7e2b8bd0430e
md <hasnainshaikh62479@gmail.com>
2024-04-28 22:19:17 +0530

    url fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ connectDB()
     const io = new Server(server, {
       pingTimeout: 60000,
       cors: {
-        origin: "*",
+        origin: "http://localhost:5173",
         // credentials: true,
       },
     });
